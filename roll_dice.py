1a877d9f7440add5be7ecc29f3f5473ecfbf5d86
Garcia McGee <agarciamcge@chsmedical.com>
2021-01-17 13:11:37 -0700

    first commit

---
+++ roll_dice.py
@@ -0,0 +1,18 @@
+import random
+
+playing = True
+
+
+while playing:
+    roll = input("Press R to roll dice or E to exit: ")
+
+    if roll.lower() == "r":
+        print(random.randint(1,6))
+        print ("Roll Again?")
+        playing = True
+        continue
+    elif roll.lower() == "e":
+        print("Thank you for playing!")
+        break
+    else:
+        print("Incorrect Entry. Please press R or E")
